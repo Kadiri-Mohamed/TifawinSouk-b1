f2c2aefcd5b0f3ed56b5b84b2aba55cb6179c56a
Kadiri <kadiri.moohamed@gmail.com>
2026-01-26 17:32:43 +0100

    feat: update CategoryFactory to include name, slug, and description generation

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
@@ -16,8 +16,11 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $name = $this->faker->words(2, true);
         return [
-            //
+            'name' => ucfirst($name),
+            'slug' => Str::slug($name),
+            'description' => $this->faker->paragraph(2),
         ];
     }
 }

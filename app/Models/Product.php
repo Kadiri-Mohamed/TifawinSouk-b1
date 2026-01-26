a07dd99f2295c60148bee427456218f9dcd0cea4
Kadiri <kadiri.moohamed@gmail.com>
2026-01-26 17:21:19 +0100

    feat: add SoftDeletes trait to Product model and define fillable attributes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory;
+    use HasFactory , SoftDeletes;
+
+    private $fillable = [
+        'nom', 'reference', 'description', 'prix', 
+        'stock', 'image', 'category_id'
+    ];
+
+    protected $casts = [
+        'stock' => 'integer',
+        'prix' => 'integer',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
